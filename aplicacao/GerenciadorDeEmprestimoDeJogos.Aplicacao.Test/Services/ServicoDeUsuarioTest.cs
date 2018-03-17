c8af7788312c8ca019038c72404ad580516e56c0
Raphael Pantaleão <raphaelpanta@gmail.com>
2018-03-17 13:04:34 -0700

    Configurando projeto de testes para serviço de aplicações

---
+++ aplicacao/GerenciadorDeEmprestimoDeJogos.Aplicacao.Test/Services/ServicoDeUsuarioTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace GerenciadorDeEmprestimoDeJogos.Aplicacao.Test.Services
+{
+    public class ServicoDeUsuarioTest
+    {
+        [Fact]
+        public void ShouldFail() {
+            throw new NotImplementedException();
+        }
+    }
+}
