b06b89d567b15d47a72804d37f63941fff0b7e19
Raphael Pantaleão <raphael.pantaleao@s2it.com.br>
2018-03-19 11:52:45 -0300

    Correção de teste de AmigosController

--- apresentacao/GerenciadorDeEmprestimoDeJogos.Mvc.Test/Controllers/AmigosControllerTest.cs
+++ apresentacao/GerenciadorDeEmprestimoDeJogos.Mvc.Test/Controllers/AmigosControllerTest.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Linq;
+using System.Security.Claims;
 using FluentAssertions;
 using GerenciadorDeEmprestimoDeJogos.Aplicacao.Services.Amigos;
 using GerenciadorDeEmprestimoDeJogos.Mvc.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -11,28 +13,42 @@ namespace GerenciadorDeEmprestimoDeJogos.Mvc.Test.Controllers
 {
     public class AmigosControllerTest
     {
+        private void SetUpContextFor(Controller c) {
+             c.ControllerContext = new ControllerContext {
+                HttpContext = new DefaultHttpContext()
+            };
+            
+            c.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new [] { 
+                new Claim("email", "raphaelpanta@gmail.com")
+            }));
+        }
         [Fact]
-        public void DeveListarAmigosNaoAdicionados() {
+        public void DeveListarAmigosNaoAdicionados()
+        {
             var servicoDeAmigos = new Mock<IServicoDeAmigos>();
 
-            var id =  Guid.NewGuid();
-            servicoDeAmigos.Setup(x => x.NaoAdicionados("raphaelpanta@gmail.com")).Returns(new [] {
+            var id = Guid.NewGuid();
+            servicoDeAmigos.Setup(x => x.NaoAdicionados(It.Is<string>(y =>  y == "raphaelpanta@gmail.com")))
+            .Returns(new[] {
                 new DadosDoAmigo { AmigoId = id, Nome = "Raphael"}
             });
 
             var controller = new AmigosController(servicoDeAmigos.Object);
-
+            SetUpContextFor(controller);
+            
+            
             var result = controller.Index() as ViewResult;
 
             result.Should().NotBeNull();
-            result.Model.Should().BeEquivalentTo(new [] {
+            result.Model.Should().BeEquivalentTo(new[] {
                 new DadosDoAmigo { AmigoId = id, Nome = "Raphael"}
             });
         }
 
         [Fact]
-        public void DeveAdicionarAmigo() {
-            
+        public void DeveAdicionarAmigo()
+        {
+
             var servicoDeAmigos = new Mock<IServicoDeAmigos>();
             var id = Guid.NewGuid();
             servicoDeAmigos.Setup(x => x.Adicionar(id, "raphaelpanta@gmail.com"));
@@ -40,7 +56,7 @@ namespace GerenciadorDeEmprestimoDeJogos.Mvc.Test.Controllers
             servicoDeAmigos.Setup(x => x.NaoAdicionados("raphaelpanta@gmail.com")).Returns(Enumerable.Empty<DadosDoAmigo>());
 
             var controller = new AmigosController(servicoDeAmigos.Object);
-
+            SetUpContextFor(controller);
             var result = controller.Index(id) as ViewResult;
 
             result.Should().NotBeNull();
