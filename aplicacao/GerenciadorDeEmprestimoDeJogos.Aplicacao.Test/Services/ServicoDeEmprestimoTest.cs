c0de46b9263ee458921cff27aa5b136278e56e9b
Raphael Pantaleão <raphael.pantaleao@s2it.com.br>
2018-03-19 14:37:13 -0300

    Correção de testes

--- aplicacao/GerenciadorDeEmprestimoDeJogos.Aplicacao.Test/Services/ServicoDeEmprestimoTest.cs
+++ aplicacao/GerenciadorDeEmprestimoDeJogos.Aplicacao.Test/Services/ServicoDeEmprestimoTest.cs
@@ -10,15 +10,18 @@ using GerenciadorDeEmprestimoDeJogos.Aplicacao.Services.Emprestimos;
 using Moq;
 using Xunit;
 
-namespace GerenciadorDeEmprestimoDeJogos.Aplicacao.Test.Services {
-    public class ServicoDeEmprestimoTest {
+namespace GerenciadorDeEmprestimoDeJogos.Aplicacao.Test.Services
+{
+    public class ServicoDeEmprestimoTest
+    {
 
         [Fact]
-        public void DeveMontadorOsDadosDoEmprestimo () {
-            var repositorio = new Mock<IRepositorioDeEmprestimo> ();
+        public void DeveMontarOsDadosDoEmprestimo()
+        {
+            var repositorio = new Mock<IRepositorioDeEmprestimo>();
 
-            repositorio.Setup (x => x.PorEmail (It.Is<string> (v => v.Equals ("raphaelpanta@gmail.com"))))
-                .Returns (new [] {
+            repositorio.Setup(x => x.PorEmail(It.Is<string>(v => v.Equals("raphaelpanta@gmail.com"))))
+                .Returns(new[] {
                     new Usuario {
                         Emprestimos = new List<Emprestimo> (),
                             Credenciais = new Credenciais {
@@ -29,40 +32,39 @@ namespace GerenciadorDeEmprestimoDeJogos.Aplicacao.Test.Services {
                     }
                 });
 
-            var servico = new ServicoDeEmprestimo (repositorio.Object) as IServicoDeEmprestimo;
+            var servico = new ServicoDeEmprestimo(repositorio.Object) as IServicoDeEmprestimo;
 
-            servico.DadosDeEmprestimo ("raphaelpanta@gmail.com")
-                .Should ()
-                .BeEquivalentTo (new DadosDoEmprestimo {
-                    Amigos = Enumerable.Empty<DadosDeAmigo> (),
-                        JogosEmprestados = Enumerable.Empty<JogoEmprestado> (),
-                        MeusJogos = Enumerable.Empty<DadosDeJogo> ()
+            servico.DadosDeEmprestimo("raphaelpanta@gmail.com")
+                .Should()
+                .BeEquivalentTo(new DadosDoEmprestimo
+                {
+                    Amigos = Enumerable.Empty<DadosDeAmigo>(),
+                    JogosEmprestados = Enumerable.Empty<JogoEmprestado>(),
+                    MeusJogos = Enumerable.Empty<DadosDeJogo>(),
+                    JogosAEmprestar = Enumerable.Empty<JogoEmprestado>()
                 });
 
-            repositorio.Verify (x => x.PorEmail (It.Is<string> (v => v.Equals ("raphaelpanta@gmail.com"))), Times.Once ());
+            repositorio.Verify(x => x.PorEmail(It.Is<string>(v => v.Equals("raphaelpanta@gmail.com"))), Times.Once());
         }
 
         [Fact]
-        public void DeveRegistrarDevolucao () {
-            var repositorio = new Mock<IRepositorioDeEmprestimo> ();
+        public void DeveRegistrarDevolucao()
+        {
+            var repositorio = new Mock<IRepositorioDeEmprestimo>();
 
-            repositorio.Setup (x => x.RegistrarDevolucao (It.IsAny<Emprestimo> ()));
-            var id = Guid.NewGuid ();
-            repositorio.Setup (x => x.EmprestimoPor (It.Is<string> (v => v.Equals ("raphaelpanta@gmail.com")), It.Is<Guid> (v => v.Equals (id))))
-                .Returns (new [] {
-                    new Emprestimo { }
-                });
+            var id = Guid.NewGuid();
+            repositorio.Setup(x => x.RegistrarDevolucao(It.Is<string>(v => v.Equals("raphaelpanta@gmail.com")), It.Is<Guid>(v => v.Equals(id))));
 
-            ClaimsIdentity claimsIdentity = new ClaimsIdentity ();
-            claimsIdentity.AddClaim (new Claim ("email", "raphaelpanta@gmail.com"));
 
-            var servico = new ServicoDeEmprestimo (repositorio.Object) as IServicoDeEmprestimo;
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim("email", "raphaelpanta@gmail.com"));
 
-            servico.DevolverJogoPorId (id, "raphaelpanta@gmail.com");
+            var servico = new ServicoDeEmprestimo(repositorio.Object) as IServicoDeEmprestimo;
 
-            repositorio.Verify (x => x.RegistrarDevolucao (It.IsAny<Emprestimo> ()));
+            servico.DevolverJogoPorId(id, "raphaelpanta@gmail.com");
 
-            repositorio.Verify (x => x.EmprestimoPor (It.Is<string> (v => v.Equals ("raphaelpanta@gmail.com")), It.Is<Guid> (v => v.Equals (id))), Times.Once ());
+            repositorio.Verify(x => x.RegistrarDevolucao(It.Is<string>(v => v.Equals("raphaelpanta@gmail.com")), It.Is<Guid>(v => v.Equals(id))), Times.Once());
+
         }
     }
 }
